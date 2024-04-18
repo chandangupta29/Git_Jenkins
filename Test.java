744f7025b7357ff0a55a8013eb84f506aa146cf6
chandangupta29 <chandan@gmail.com>
2024-04-19 01:17:15 +0530

    working

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
-public class Main{
-	public static void main(String[] args){
-		Sysout.println(x:"hi this is me");
-	}
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("hi this is me");
+    }
 }
