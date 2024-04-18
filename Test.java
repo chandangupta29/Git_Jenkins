fb464abeeedd108af4966eaf932bb35d32eeb0b3
chandangupta29 <chandan@gmail.com>
2024-04-18 22:37:51 +0530

    changes3

--- Test.java
+++ Test.java
@@ -1,14 +1,5 @@
-// Java program to take an integer
-// as input and print it
-import java.io.*;
-import java.util.Scanner;
-
-// Driver Class
-class GFG {
-	// main function
-	public static void main(String[] args)
-	{
-		 
-		System.out.println("Hi");
+public class Main{
+	public static void main(String[] args){
+		Sysout.println(x:"hi this is me");
 	}
 }
