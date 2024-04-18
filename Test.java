6332b84202a1253c680bf745619aa1b3c3e34c30
chandangupta29 <chandan@gmail.com>
2024-04-18 22:26:25 +0530

    test

--- Test.java
+++ Test.java
@@ -8,20 +8,7 @@ class GFG {
 	// main function
 	public static void main(String[] args)
 	{
-		// Declare the variables
-		int num;
-
-		// Input the integer
-		System.out.println("Enter the integer: ");
-
-		// Create Scanner object
-		Scanner s = new Scanner(System.in);
-
-		// Read the next integer from the screen
-		num = s.nextInt();
-
-		// Display the integer
-		System.out.println("Entered integer is: "
-						+ num);
+		 
+		System.out.println("Hi";
 	}
 }
